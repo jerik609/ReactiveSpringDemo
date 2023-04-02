67a292adfc2803811a565a692d352313cbec142e
Michal <117671671+jerik609@users.noreply.github.com>
2023-04-02 20:11:04 +0200

    lesson 2 - some comments

--- src/main/java/com/example/ReactiveSpringDemo/lesson_2/RoutesConfiguration.java
+++ src/main/java/com/example/ReactiveSpringDemo/lesson_2/RoutesConfiguration.java
@@ -5,6 +5,7 @@ import com.example.ReactiveSpringDemo.lesson_2.services.ProductService2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -20,8 +21,22 @@ public class RoutesConfiguration {
     @Bean
     public RouterFunction<ServerResponse> productRoutes() {
         return route()
-                .GET("/product2", request -> ok().body(productService2.getAll(), Product.class))
+                .GET("/product2", request -> ok()
+                        .contentType(MediaType.TEXT_EVENT_STREAM) // but in chrome it worked even without it, so chrome is probably able to handle this somehow properly by now?
+                        // actually the content type changes the behavior
+                        // where I'd see the json list slowly populating, with the "correct" content type, the data is arriving as text one-by-one
+                        .body(productService2.getAll(), Product.class))
                 // more routes can be added here
                 .build();
+
+        // what is the benefit?
+        // - releasing resources when we're waiting (a thread)
+        // - more responsive application ... deliver the values as they are ready, not waiting for all of them being ready
+        // - making threads when they are waiting for something
+
+        // @Async vs reactive - i.e. what really is spring's @Async? https://www.baeldung.com/spring-mvc-async-vs-webflux
+        // ok, so now only reactive backpressure remains ...
+
     }
+
 }
